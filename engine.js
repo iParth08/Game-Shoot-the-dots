7a065299237569042002efc30d1b6d95cea520b3
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-03-13 23:51:04 +0530

    Particle Scatter

--- engine.js
+++ engine.js
@@ -1,4 +1,4 @@
-// Canvas Setting on DOM LOAD and Resize //todo:
+// ------------------------------------------Setting Game ENVIRONMENT -- Canvas and Context------------------------
 const gamePanel = document.querySelector(".gamePanel");
 const canvas = document.createElement("canvas");
 gamePanel.appendChild(canvas);
@@ -8,15 +8,30 @@ canvas.height = innerHeight;
 // create context
 const context = canvas.getContext("2d");
 
-// ---------------------------------------------------------------------------------------------------------
-// GAME SETTINGS
+// ---------------------------------------- GAME SETTINGS and Variables ----------------------------
 
 // Difficulty setting and Score Points/Highscore
-let difficulty = 1;
-let highScore = 101;
-let levelUp = 5;
+
 let score = 0;
-let base = 0.2;
+let highScore = {
+  Easy: 100,
+  Medium: 50,
+  Hard: 25,
+  Insane: 5,
+};
+
+let difficulty = 1;
+let levelUp = 5;
+let base = 0.5;
+let spawnRate = 1000;
+let impact = 6;
+let spawnControl;
+let diffLevel;
+
+const weapons = [];
+const enemies = [];
+const particles = [];
+
 const form = document.querySelector("form");
 const scoreboard = document.querySelector(".scorecard");
 const levelScore = document.querySelector(".highscore");
@@ -29,30 +44,34 @@ document.querySelector("#play").addEventListener("click", (e) => {
   levelScore.style.display = "none";
   scoreboard.style.display = "block";
 
-  const userVal = document.querySelector("#diff").value;
+  diffLevel = document.querySelector("#diff").value;
 
-  if (userVal === "Easy") {
-    difficulty = 2;
-    setInterval(enemySpawn, 6000);
-  } else if (userVal === "Medium") {
-    difficulty = 3;
-    setInterval(enemySpawn, 4000);
-  } else if (userVal === "Hard") {
-    difficulty = 4;
-    setInterval(enemySpawn, 3000);
-  } else if (userVal === "Insane") {
-    difficulty = 5;
-    setInterval(enemySpawn, 2000);
+  switch (diffLevel) {
+    case "Easy":
+      difficulty = 2;
+      spawnRate = 3000;
+      break;
+    case "Medium":
+      difficulty = 3;
+      spawnRate = 3000;
+      break;
+    case "Hard":
+      difficulty = 4;
+      spawnRate = 2000;
+      break;
+    case "Insane":
+      difficulty = 5;
+      spawnRate = 2000;
+      break;
   }
 
-  levelScore.textContent = `${userVal} : ${highScore}`;
-  // debugg :
-  console.log(enemies);
+  // Clear Canvas
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  spawnControl = setInterval(enemySpawn, spawnRate);
   animation();
 });
 
-// ----------------------------------------------------------------------------------------------------
-// CLASS CREATION FOR ALL OBJECTS
+// ------------------------------CLASS CREATION FOR ALL OBJECTS-------------------------------------------
 
 // Super Parent DOT {BASE CLASS}
 class DOT {
@@ -100,6 +119,37 @@ class Enemy extends Weapon {
   }
 }
 
+// 4. Particle Class
+class Particle extends Weapon {
+  constructor(x, y, radius, color, velocity) {
+    super(x, y, radius, color, velocity);
+    this.alpha = 1;
+    this.friction = 0.97;
+  }
+
+  draw() {
+    context.save();
+    context.globalAlpha = this.alpha;
+    super.draw();
+    context.restore();
+  }
+  update() {
+    this.draw();
+
+    // slow down its velocity
+    this.velocity.x *= this.friction;
+    this.velocity.y *= this.friction;
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.alpha -= 0.01; //fade out the particles {1......0}
+
+    if (this.alpha <= 0) {
+      particles.splice(particles.indexOf(this), 1);
+    }
+  }
+}
+
 // ---------------------------------------------------------------------------------------------
 // COMPONENT CREATION {OBJECTS}
 
@@ -121,8 +171,7 @@ player.draw();
 
 // Player Pulse, Defence Active //! Additional Features
 
-// --------------------------------------------------------------------------------------------
-const enemies = [];
+// -------------------------------------Enemies Behaviours------------------------------------------------------
 
 // Enemy Spawning
 const enemySpawn = () => {
@@ -162,9 +211,9 @@ const enemySpawn = () => {
     new Enemy(random.x, random.y, enemySize, enemyColor, enemyVelocity)
   );
 };
-// ----------------------------------------------------------------------------------------------
-//Player Actions and Weapons
-const weapons = [];
+// --------------------------------------Player Actions in Game----------------------------------------------------
+
+// Light Weapon (Left Click)
 canvas.addEventListener("click", (event) => {
   const clickAngle = Math.atan2(
     event.clientY - playerPosition.y,
@@ -188,8 +237,7 @@ canvas.addEventListener("click", (event) => {
   );
 });
 
-// ---------------------------------------------------------------------------------------------------
-// Game Animation Handler
+// -------------------------------------------Game Engine Controls----------------------------------------
 let animationID;
 function animation() {
   animationID = requestAnimationFrame(animation);
@@ -198,6 +246,13 @@ function animation() {
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   player.draw();
+
+  // Update particles movement
+  particles.forEach((particle) => {
+    particle.update();
+  });
+
+  // Update weapons movement
   weapons.forEach((weapon, weaponIndex) => {
     weapon.update();
 
@@ -214,6 +269,7 @@ function animation() {
     }
   });
 
+  // Update enemies movement
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
 
@@ -238,15 +294,18 @@ function animation() {
 
       //if circle is close enough to touch the periphery
       if (closenessWeaponEnemy - weapon.radius - enemy.radius < 1) {
-        if (enemy.radius > 11) {
-          //set smooth transition for radius reduce
+        scoreUpdate(5); //! Testing feature
+
+        if (enemy.radius > 16) {
+          //set smooth transition for radius reduce [REDUCE]
           gsap.to(enemy, { radius: enemy.radius - 10 });
 
           setTimeout(() => {
             weapons.splice(weaponIndex, 1);
           }, 0);
         } else {
-          // remove weapon and enemy, quickly
+          // remove weapon and enemy, quickly [KILL]
+          particleGenerator(weapon, enemy);
           setTimeout(() => {
             weapons.splice(weaponIndex, 1);
             enemies.splice(enemyIndex, 1);
@@ -257,20 +316,49 @@ function animation() {
   });
 }
 
+const particleGenerator = (weapon, enemy) => {
+  // Particles Effects Movement generation
+  for (let i = 0; i < 20; i++) {
+    particles.push(
+      new Particle(weapon.x, weapon.y, Math.random() * 3, enemy.color, {
+        x: (Math.random() - 0.5) * (Math.random() * impact), // think of it as *impact
+        y: (Math.random() - 0.5) * (Math.random() * impact),
+      })
+    );
+  }
+};
 const gameOver = () => {
   score = 0;
   form.style.display = "flex";
   title.style.display = "block";
   levelScore.style.display = "block";
   scoreboard.style.display = "none";
-  // empty arrays
+
+  // Update Highscore
+  highScorer(score); //! Testing feature
+
+  // empty arrays and stop new enemy spawn
+  clearInterval(spawnControl);
   enemies.splice(0, enemies.length);
   weapons.splice(0, weapons.length);
-
-  console.log(enemies, weapons);
 };
 
-// !issue 1 : too many enemy attack at once in second start
+// NOT WORKING AS I WANTED
+const scoreUpdate = (value) => {
+  score += value;
+  scoreboard.innerHTML = `<h2>Score: <span class="score">${score}</span></h2>`;
+};
+
+const highScorer = (scored) => {
+  if (scored > highScore[diffLevel]) {
+    highScore[diffLevel] = scored;
+  }
+
+  levelScore.innerHTML = `${diffLevel}: ${highScore[diffLevel]}`;
+  console.table([diffLevel, highScore[diffLevel]]);
+};
+
+// !issue 1 : too many enemy attack at once in second start => terminate setInterval
 // !issue 2 : speed and spawn rate
 // !issue 3 : newGame and GameOver function
 // !issue 4 : Enemy radius and Score
